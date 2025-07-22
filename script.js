7985ce85136cee8ffcbef71596c258aa0fd58d1c
zloba010 <59543137+zloba010@users.noreply.github.com>
2025-07-22 08:19:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -184,7 +184,6 @@ function onSubmitSearch(event) {
  } else if (query.startsWith("/qr")) {
  window.open('https://zloba010.github.io/Ruaskerland/qrkod.html', '_blank');
  } else if (query.startsWith("/kliker")) {
-     debbuger;
  window.open('https://github.io/Ruaskerland/kliker.html', '_blank');
  } else if (query.startsWith("/radio")) {
  if (!radioActive) {
